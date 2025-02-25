e8d41fdce7c13821b40fa611216f6d2ae9c03ad5
Jack Harper <harps912@gmail.com>
2025-02-25 21:28:59 +0000

    Update consume.py

--- src/saluki/consume.py
+++ src/saluki/consume.py
@@ -33,6 +33,7 @@ def consume(
             "auto.offset.reset": "latest",
             "enable.auto.offset.store": False,
             "enable.auto.commit": False,
+            "metadata.max.age.ms": 6000
         }
     )
 
