816783ab0c1cedad1915169999cb617841c05ac3
Jack Harper <jack.harper@stfc.ac.uk>
2026-03-20 16:05:39 +0000

    run start, but without nexus structure

--- src/howl.rs
+++ src/howl.rs
@@ -1,14 +1,67 @@
-use std::time::SystemTime;
+use std::thread;
+use std::time::{Duration, SystemTime};
 
 use flatbuffers::FlatBufferBuilder;
+use isis_streaming_data_types::flatbuffers_generated::det_spec_map_df12::SPECTRA_DETECTOR_MAPPING_IDENTIFIER;
 use isis_streaming_data_types::flatbuffers_generated::events_ev44::{
-    Event44Message, Event44MessageArgs, Event44MessageBuilder, finish_event_44_message_buffer,
+    Event44Message, Event44MessageArgs, finish_event_44_message_buffer,
 };
-use log::debug;
+use isis_streaming_data_types::flatbuffers_generated::run_start_pl72::{
+    RunStart, RunStartArgs, SpectraDetectorMapping, SpectraDetectorMappingArgs,
+    finish_run_start_buffer,
+};
+use isis_streaming_data_types::flatbuffers_generated::run_stop_6s4t::{
+    RunStop, RunStopArgs, finish_run_stop_buffer
+};
+use log::{debug, warn};
 use rand::{Rng, RngExt, rng};
 use rand_distr::{Distribution, Normal};
+use rdkafka::ClientConfig;
+use rdkafka::producer::{BaseProducer, BaseRecord};
+use uuid::Uuid;
 
-fn generate_run_start() {}
+fn generate_run_start<'a>(
+    fbb: &'a mut FlatBufferBuilder<'_>,
+    det_max: i32,
+    topic_prefix: String,
+    job_id: String,
+) -> &'a [u8] {
+    fbb.reset();
+    let args = SpectraDetectorMappingArgs {
+        spectrum: Some(fbb.create_vector(&(0..=det_max).collect::<Vec<_>>())),
+        detector_id: Some(fbb.create_vector(&(0..=det_max).collect::<Vec<_>>())),
+        n_spectra: det_max,
+    };
+
+    let det_spec_map_buf = SpectraDetectorMapping::create(fbb, &args);
+    let file_name = Uuid::new_v4().to_string();
+    let run_name = format!("saluki-howl-{}", Uuid::new_v4());
+
+    let start_time = SystemTime::now()
+        .duration_since(SystemTime::UNIX_EPOCH)
+        .unwrap()
+        .as_millis();
+
+    let run_start_args = RunStartArgs {
+        start_time: start_time as u64,
+        stop_time: 0, // TODO check this
+        run_name: Some(fbb.create_string(run_name.as_str())),
+        instrument_name: Some(fbb.create_string("saluki-howl")),
+        nexus_structure: Some(fbb.create_string("")), // TODO
+        job_id: Some(fbb.create_string(job_id.as_str())),
+        broker: None,
+        service_id: None,
+        filename: Some(fbb.create_string(file_name.as_str())),
+        n_periods: 1,
+        detector_spectrum_map: Some(det_spec_map_buf),
+        metadata: None,
+        control_topic: None,
+    };
+    let run_start_buf = RunStart::create(fbb, &run_start_args);
+
+    finish_run_start_buffer(fbb, run_start_buf);
+    fbb.finished_data()
+}
 
 fn generate_run_stop() {}
 
@@ -24,6 +77,7 @@ fn generate_fake_events<'a>(
     det_max: i32,
     timestamp: f32,
 ) -> &'a [u8] {
+    fbb.reset();
     let mut rng = rand::rng();
 
     let det_ids: Vec<i32> = (0..events_per_message)
@@ -64,7 +118,6 @@ pub fn howl(
 
     let mut fbb = FlatBufferBuilder::new();
 
-    // TODO work this out properly when we have streaming-data-types
     let now = SystemTime::now()
         .duration_since(SystemTime::UNIX_EPOCH)
         .unwrap()
@@ -87,17 +140,62 @@ pub fn howl(
     let rate_bytes_per_sec = ev44_size * messages_per_frame * frames_per_second;
     debug!("bytes per second: {rate_bytes_per_sec}");
 
-    let rate_mbit_per_sec: f32  = (rate_bytes_per_sec as f32 / 1024f32.powf(2.0)) * 8.0;
-    let rate_mebibits_per_sec = rate_mbit_per_sec/8.0;
+    let rate_mbit_per_sec: f32 = (rate_bytes_per_sec as f32 / 1024f32.powf(2.0)) * 8.0;
+    let rate_mebibits_per_sec = rate_mbit_per_sec / 8.0;
     debug!("rate mbit per sec: {rate_mbit_per_sec}");
     println!(
         "Attempting to simulate data rate: {rate_mbit_per_sec:.3} Mbit/s ({rate_mebibits_per_sec:.3} MiB/s)"
     );
     println!("Each ev44 is {ev44_size} bytes");
 
-    // produce run start
+    let producer: BaseProducer = ClientConfig::new()
+        .set("bootstrap.servers", broker)
+        .create()
+        .expect("Producer creation error");
 
-    // loop:
-    //  produce ev44s
-    //  produce run stop and starts if frames-per-run specified
+    let mut current_job_id = Uuid::new_v4().to_string();
+
+    let runinfo_topic = format!("{topic_prefix}_runInfo");
+    let events_topic = format!("{topic_prefix}_rawEvents");
+
+    producer
+        .send(
+            BaseRecord::to(runinfo_topic.as_str())
+                .key("")
+                .payload(generate_run_start(
+                    &mut fbb,
+                    det_max,
+                    topic_prefix,
+                    current_job_id,
+                )),
+        )
+        .expect("Failed to enqueue run start message");
+
+    let target_frame_time = Duration::from_secs((1 / frames_per_second) as u64);
+
+    let mut frames: u64 = 0;
+
+    let mut target_time = SystemTime::now()
+        .duration_since(SystemTime::UNIX_EPOCH)
+        .unwrap();
+    loop {
+        target_time += target_frame_time;
+        frames += 1;
+
+        // current_job_id = produce_messages()
+
+        let sleep_time = target_time
+            - SystemTime::now()
+                .duration_since(SystemTime::UNIX_EPOCH)
+                .unwrap();
+
+        if sleep_time.as_millis() > 0 {
+            thread::sleep(sleep_time);
+        } else {
+            warn!(
+                "saluki howl running {} seconds behind schedule",
+                sleep_time.as_secs()
+            )
+        }
+    }
 }
